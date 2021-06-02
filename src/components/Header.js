fdf0160c5195ae85a8bea921b7874907b75cbb24
meriemkhadraoui <miyakhad@gmail.com>
2021-06-02 14:20:45 +0200

    instagram app header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,32 @@
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="content-one">
+        <div className="imag">
+          <img src="" />
+        </div>
+        <div className="follow">
+          <span>
+            <h1>natgeo</h1> <button type="submit" value="Follow " />
+            <h1>...</h1>
+          </span>
+          <span>
+            {" "}
+            <h3>25,035 posts </h3> <h3>165m followers </h3>{" "}
+            <h3>131 following </h3>{" "}
+          </span>
+          <span>
+            <h3>National Geographic</h3>{" "}
+            <h4>
+              Experience the world through the eyes of National Geographic
+              photographers.
+            </h4>
+            <a href="https://nationalgeographic.like2buy.curalate.com/c/gallery/like2buy?id=71xx76mM&l=like2buy" />
+          </span>
+        </div>
+      </div>
+      <div className="content-two"></div>
+    </div>
+  );
 };
 export default Header;
