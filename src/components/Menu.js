247bd02098867f862ea969a33d212ac8737344e7
meriemkhadraoui <miyakhad@gmail.com>
2021-06-03 21:35:07 +0200

    instagram app header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompass } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 const Menu = () => {
+  const compass = <FontAwesomeIcon icon={faCompass} />;
+  const home = <FontAwesomeIcon icon={faHome} />;
+  const paperPlane = <FontAwesomeIcon icon={faPaperPlane} />;
+  const heart = <FontAwesomeIcon icon={faHeart} />;
   return (
     <div className="menu">
       <span className="insta">Instagram</span>
 
       <input className="input" type="text" placeholder=" search" />
       <div className="icons">
-        <span> </span>
+        <span>{compass} </span>
 
-        <span> </span>
+        <span>{home} </span>
 
-        <span> </span>
+        <span> {paperPlane}</span>
 
-        <span></span>
+        <span>{heart}</span>
 
         <span className="profilPhoto"></span>
       </div>
